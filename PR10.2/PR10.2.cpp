1ce95f4f19bc6ddd6e117e9d53c37393f8730342
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-12-07 22:45:49 +0200

    fefr

---
+++ PR10.2/PR10.2.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+void rewrite_info(string output, string input) {
+	ifstream file(input);
+	string line;
+	bool isSpace = false;
+	if (file.is_open()) {
+		ofstream outfile(output);
+		if (file.is_open()) {
+			while (getline(file, line)) {
+				for (int i = 0; i < line.size(); i++) {
+					if (!isSpace) {
+						if (line[i] >= 48 && line[i] <= 57 || line[i] >= 97 && line[i] <= 122) {
+							outfile << line[i];
+						}
+						else if (line[i] >= 65 && line[i] <= 90) {
+							outfile << char(line[i] + 32);
+						}
+						else if (line[i] == ' ') {
+							outfile << line[i];
+							isSpace = true;
+						}
+					}
+					else {
+						outfile << line[i];
+					}
+					
+				}
+			}
+			outfile.close();
+		}
+		else {
+			cout << "Error while opening output file" << endl;
+		}
+		file.close();
+	}
+	else {
+		cout << "Error while opening input file" << endl;
+	}
+	
+}
+
+int main() {
+	rewrite_info("output.txt", "text.txt");
+	return 0;
+}
---
+++ UnitTest10.2/UnitTest10.2.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR10.2/PR10.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest102
+{
+	TEST_CLASS(UnitTest102)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			rewrite_info("output.txt", "text.txt");
+			string expected = "a1sdf3ghww4e6rt AScvjk456-+-=)(hjkb)";
+			string actual="";
+			ifstream file("output.txt");
+			if (file.is_open()) {
+				getline(file, actual);
+			}
+			file.close();
+			Assert::AreEqual(expected, actual);
+		}
+	};
+}
